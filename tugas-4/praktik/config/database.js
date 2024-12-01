4dbb11d7b16c0fdfb3511e89bfe6ee59d2f8ee6e
farhanisty <farhannivtasija@gmail.com>
2024-12-01 22:34:52 +0700

    create database connection

--- tugas-4/praktik/config/database.js
+++ tugas-4/praktik/config/database.js
@@ -1,7 +1,10 @@
-module.exports = {
-  multipleStatements: true,
-  host: "localhost",
+import mysql from "mysql2";
+
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
   user: "root",
   password: "",
   database: "nwind",
-};
+});
+
+export default connection;
